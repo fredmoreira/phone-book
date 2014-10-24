e2eda6b12ad5206269ca834e608f6170866bc036
mtbitencourt <leandro@minutrade.com>
2014-10-24 09:08:23 -0200

    exportado o módulo principal 'app' para facilitar os tests da API'

--- server.js
+++ server.js
@@ -6,16 +6,32 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 // MongoDB
-mongoose.connect('mongodb://localhost/phone_book');
+mongoose.connect('mongodb://localhost/phone_book', function(err) {
+    if (err) {
+        console.error("Could not connect to DB: %s", err);
+        process.exit(1);
+    }
+});
 
 // Express
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 // Rotas
 app.use('/api', require('./routes/api'));
 
 // Inicia o servidor na porta 5000
-app.listen(5000);
-console.log('API Rodando na porta 5000!');
+
+app.set('port', process.env.PORT || 5000);
+
+var server = app.listen(app.get('port'), function(err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.info('Express server listening on port: %d', server.address().port);
+});
+
+module.exports = app;
