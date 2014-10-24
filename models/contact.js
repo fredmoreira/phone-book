cc88a43b82133b187141764236606670af52e6c0
Frederico Moreira <fredmacc@gmail.com>
2014-10-24 01:35:53 -0200

    Correção no Schema
    Correção no Modal

--- models/contact.js
+++ models/contact.js
@@ -4,11 +4,11 @@ var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 // Schema
-var productSchema = new mongoose.Schema({
+var contactSchema = new mongoose.Schema({
     name: String,
-    mobilephone: Number,
-    homephone: Number
+    mobilephone: String,
+    homephone: String
 });
 
 // Retorna model
-module.exports = restful.model('Contacts', productSchema);
+module.exports = restful.model('Contacts', contactSchema);
