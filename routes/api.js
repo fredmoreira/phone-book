4f9e525a2ecca711df5b8d27c9f09fc1b38422ec
Frederico Moreira <fredmacc@gmail.com>
2014-10-26 20:50:13 -0200

    Atualização do server para filtrar get pelo name

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,25 @@ var Contact = require('../models/contact');
 
 // Rotas
 Contact.methods(['get', 'put', 'post', 'delete']);
+
+router.get('/contacts/names/:name', function (req, res) {
+    Contact.find({
+        name: req.params.name
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+
+        if (contact.length == 0) {
+            res.status(404);
+            res.send('Name not found');
+        } else
+            res.send(contact);
+    });
+});
+
+//Registrando rotas
 Contact.register(router, '/contacts');
+Contact.register(router, '/contacts/names');
 
 // Retorna rota
 module.exports = router;
