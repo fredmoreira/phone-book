11262e36a449665db9c0031c102ace12411427a3
Frederico Moreira <fredmacc@gmail.com>
2014-10-31 01:26:32 -0200

    Inclusao do metodo put

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ API REST simples para experimentar frameworks de automação de testes de api's
 
 * GET 	  /api/contacts 		 --> Consulta todos os contatos
 * GET 	  /api/contacts/:id    --> Consulta contato especificado pelo _id (Id no MongoDB)
-* GET	  /api/contacts?parametro=  --> Consulta contato filtrando pelo parametro(name,mobilephone,homephone)
+* GET	  /api/contacts?parametro=    Consulta contato filtrando pelo parametro(name,mobilephone,homephone)
 * POST 	  /api/contacts 		 --> Adiciona novo contato
 * DELETE  /api/contacts/:id	  --> Deleta contato especificado pelo _id (Id no MongoDB)
 
--- routes/api.js
+++ routes/api.js
@@ -18,41 +18,59 @@ module.exports = function(app) {
     });
 
     app.post('/api/contacts', function(req, res) {
-            var name = req.body.name,
-                mobilephone = req.body.mobilephone,
-                homephone = req.body.homephone;
+        var name = req.body.name,
+            mobilephone = req.body.mobilephone,
+            homephone = req.body.homephone;
 
-            if (!name || !mobilephone) {
-                res.status(400).send('Parâmetro(s) invalido(s)').end();
-            } else {
-                var obj = new Contact({
-                    name: name,
-                    mobilephone: mobilephone,
-                    homephone: homephone
-                });
+        if (!name || !mobilephone) {
+            res.status(400).send('Parâmetro(s) invalido(s)').end();
+        } else {
+            var obj = new Contact({
+                name: name,
+                mobilephone: mobilephone,
+                homephone: homephone
+            });
 
-                obj.save(function(err, data) {
-                    if (err) {
-                        res.send(500);
-                    } else {
-                        res.json(data);
-                        res.status(201).end();
-                    }
-                });
-            };
-    });        
-    
+            obj.save(function(err, data) {
+                if (err) {
+                    res.send(500);
+                } else {
+                    res.json(data);
+                    res.status(201).end();
+                }
+            });
+        };
+    });
+
 
     app.delete('/api/contacts/:id', function(req, res) {
-        var id ={_id : req.params.id};
+        var id = {
+            _id: req.params.id
+        };
+        Contact.remove(id, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.status(400).end();
+            } else
+                res.status(204).end();
 
-            Contact.remove(id, function(err , data) {
-                if (err){
-                    console.log(err);
-                    res.status(400).end();
-                }else
-                    res.status(204).end();
+        });
+    });
 
-            });
+    app.put('/api/contacts/:id', function(req, res) {
+        var id = {
+            _id: req.params.id
+        };
+        Contact.update(id, {
+            name: req.body.name
+        }, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.status(400).end();
+            } else {
+                var result = JSON.stringify(res.body)
+                res.status(200).send(result).end();
+            }
+        });
     });
 }
